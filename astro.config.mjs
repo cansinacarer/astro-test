9cfc710173e89d65b7d7077f034ad5d3e61f820a
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-05-25 22:23:07 -0400

    feat: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://cansinacarer.github.io",
-	base: "/astro-test",
+	site: "https://astro-test.cansin.net",
+	base: "/",
 });
