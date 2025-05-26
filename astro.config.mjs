94e9972c0701d2920150de83d123b1f5f8484470
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-05-26 02:32:51 -0400

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,35 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-test.cansin.net",
-	base: "/",
-	markdown: {
-		layouts: {
-			// default: "./src/layouts/Layout.astro",
-			default: "./src/layouts/Layout.astro",
-			blog: "./src/layouts/BlogLayout.astro",
-		},
-		syntaxHighlight: "shiki",
-		shikiConfig: {
-			theme: "github-light",
-			wrap: true,
-			langs: [
-				"javascript",
-				"typescript",
-				"css",
-				"html",
-				"json",
-				"markdown",
-			],
-		},
+  site: "https://astro-test.cansin.net",
+  base: "/",
+
+  markdown: {
+      layouts: {
+          // default: "./src/layouts/Layout.astro",
+          default: "./src/layouts/Layout.astro",
+          blog: "./src/layouts/BlogLayout.astro",
+      },
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+          theme: "github-light",
+          wrap: true,
+          langs: [
+              "javascript",
+              "typescript",
+              "css",
+              "html",
+              "json",
+              "markdown",
+          ],
+      },
 	},
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
