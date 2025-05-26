27a59a4749ac972625bb6a55509fdbe3aa941e55
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-05-26 18:22:31 -0400

    feat: add daisyui and some test elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,31 +5,30 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-test.cansin.net",
-  base: "/",
+	site: "https://astro-test.cansin.net",
+	base: "/",
 
-  markdown: {
-      layouts: {
-          // default: "./src/layouts/Layout.astro",
-          default: "./src/layouts/Layout.astro",
-          blog: "./src/layouts/BlogLayout.astro",
-      },
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-          theme: "github-light",
-          wrap: true,
-          langs: [
-              "javascript",
-              "typescript",
-              "css",
-              "html",
-              "json",
-              "markdown",
-          ],
-      },
+	markdown: {
+		layouts: {
+			default: "./src/layouts/Layout.astro",
+			// blog: "./src/layouts/BlogLayout.astro", // You can also create specific layouts
+		},
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			theme: "github-light",
+			wrap: true,
+			langs: [
+				"javascript",
+				"typescript",
+				"css",
+				"html",
+				"json",
+				"markdown",
+			],
+		},
 	},
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
